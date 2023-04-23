2d7a1e611227e07537c53a248e8d915937c247ee
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2023-04-23 21:25:14 +0700

    Refactor: home page

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:eco_quiz/screens/learning/learning_menu/learning_menu_page.dart';
 import 'package:eco_quiz/screens/test/test_list/test_list_page.dart';
 import 'package:eco_quiz/screens/test/test_result/test_result_page.dart';
-import 'package:eco_quiz/widgets/app_bar.dart';
 import 'package:eco_quiz/widgets/menu_button.dart';
 import 'package:flutter/material.dart';
 
@@ -12,15 +11,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(context, "Eco Quiz", false) as PreferredSizeWidget,
       body: ListView(
         children: <Widget>[
-          menuButton(0.3, "Learning", LearningMenuPage.route,
-              Icons.desktop_windows, 'assets/img/1.jpg', context),
-          menuButton(0.3, "Test", TestListPage.route, Icons.edit,
-              'assets/img/2.jpg', context),
-          menuButton(0.3, "Result", TestResultPage.route, Icons.content_paste,
-              'assets/img/3.jpeg', context),
+          menuButton("Learning", LearningMenuPage.route, Icons.desktop_windows,
+              Colors.blue, context),
+          menuButton(
+              "Test", TestListPage.route, Icons.edit, Colors.green, context),
+          menuButton("Result", TestResultPage.route, Icons.content_paste,
+              Colors.yellow, context),
         ],
       ),
     );
--- lib/widgets/menu_button.dart
+++ lib/widgets/menu_button.dart
@@ -1,53 +1,55 @@
 import 'package:flutter/material.dart';
 
-Widget menuButton(double heightPercentage, String name, String route,
-    IconData icon, String image, BuildContext context) {
+Widget menuButton(String name, String route, IconData icon, Color color,
+    BuildContext context) {
   var mediaQuerySize = MediaQuery.of(context).size;
 
   return GestureDetector(
     child: Container(
-      height: heightPercentage * mediaQuerySize.height,
+      height: mediaQuerySize.height / 3,
       width: mediaQuerySize.width,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(image),
-          fit: BoxFit.fitWidth,
-        ),
-      ),
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Icon(
-                  icon,
-                  color: Colors.black54,
-                  size: 0.2115 * mediaQuerySize.width,
-                ),
-                Icon(
-                  icon,
-                  color: Colors.white,
-                  size: 0.2 * mediaQuerySize.width,
-                ),
-              ],
-            ),
-            SizedBox(width: 0.05 * mediaQuerySize.width),
-            Text(
-              name,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 0.1 * mediaQuerySize.width,
-                  fontWeight: FontWeight.w700,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black,
-                      blurRadius: 8.0,
-                    )
-                  ]),
-            ),
-          ],
+      padding: EdgeInsets.symmetric(
+          vertical: 0.005 * mediaQuerySize.height,
+          horizontal: 0.005 * mediaQuerySize.width),
+      child: Container(
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10)),
+        height: mediaQuerySize.height * 0.3,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Icon(
+                    icon,
+                    color: Colors.black54,
+                    size: 0.2115 * mediaQuerySize.width,
+                  ),
+                  Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 0.2 * mediaQuerySize.width,
+                  ),
+                ],
+              ),
+              SizedBox(width: 0.05 * mediaQuerySize.width),
+              Text(
+                name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 0.1 * mediaQuerySize.width,
+                    fontWeight: FontWeight.w700,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        blurRadius: 8.0,
+                      )
+                    ]),
+              ),
+            ],
+          ),
         ),
       ),
     ),
